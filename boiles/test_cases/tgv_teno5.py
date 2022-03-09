06124d24165b8090214d38d1124844ae2e341c63
yiqi <yiqi.feng@tum.de>
2022-03-09 22:01:57 +0100

    update tgv upper

--- boiles/test_cases/tgv_teno5.py
+++ boiles/test_cases/tgv_teno5.py
@@ -3,11 +3,11 @@ from .tgv import TGV
 
 
 class TGVTENO5(TGV):
-    training_data = [
-        "data/tgvteno5_st_64_20s_wn3_1234.csv"
-    ]
+    # training_data = [
+    #     "data/tgvteno5_st_64_20s_wn3_1234.csv"
+    # ]
 
-    lowest_error_from_initial = 3.65
-    highest_error_from_initial = 8.98
+    lowest_error_from_initial = 1.4468
+    highest_error_from_initial = 13.8662
 
     # mean_prior = True
