0ef3806e7f4f9e7f67699e15443f44d05f7704b1
yiqi <yiqi.feng@tum.de>
2022-05-02 00:43:14 +0200

    change the upper bound to 0.5 to stabilize the GP fitting

--- boiles/test_cases/sod_60/sod_disper_60.py
+++ boiles/test_cases/sod_60/sod_disper_60.py
@@ -4,17 +4,17 @@ from .sod_base_60 import Sod60
 bound = {
     "density": (0.05176, 0.32190),
     "pressure": (0.06333, 0.29718),
-    "velocity": (0.08593, 0.90981)
+    "velocity": (0.09284, 0.96046)
 }
 
 class SodDisper60(Sod60):
     training_data = [
-        "data/sod_60_sum_roe_st_teno_1234.csv"
+        # "data/sod_60_sum_roe_st_teno_1234.csv"
     ]
 
     name = "disper"
     lowest_error_from_initial = 0
-    highest_error_from_initial = 3
+    highest_error_from_initial = 0.5
     ref_point = get_ref_point(lowest_error_from_initial, highest_error_from_initial, 0.5)
     # sensitivity_control = "noise_level"
     # sensitivity_control = "prior_distribution"
