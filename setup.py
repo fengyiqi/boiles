13b51d0e5bc585decede7e881176053b53c31ef0
yiqi <yiqi.feng@tum.de>
2022-05-03 17:39:20 +0200

    include json data when installing

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 
-setup(name='boiles',
+setup(
+      name='boiles',
       version='0.2',
       description='a package that helps building ILES scheme using Bayesian optimization based on gpytorch, botorch and ax',
       url='http://github.com/fengyiqi/boiles.git',
@@ -9,4 +10,6 @@ setup(name='boiles',
       author_email='yiqi.feng@hotmail.com',
       license='Apache',
       packages=find_packages(),
-      )
+      package_data={"": ["shuosher_ref.json"]},
+      include_package_data=True
+)
